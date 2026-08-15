43edb39384a212014ceaf71df4f199c1daf39b42
maxsimka <maxsimka4824@gmail.com>
2026-08-15 12:32:46 +0300

    some features

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-use std::{error::Error, fs::File, io::{self, Read, Write}, net::{IpAddr, Ipv4Addr, SocketAddr, TcpListener, TcpStream}, sync::mpsc, thread, time::Duration};
-use local_ip_address::local_ip;
+use std::{ fs::{self, File}, io::{self, Read, Write}, net::{TcpListener, TcpStream}, path::{Path, PathBuf}, sync::mpsc, thread};
 use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
 
 fn hello() -> Result<(), Box<dyn std::error::Error>> {
@@ -13,7 +12,8 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
     let service_type = "_localTransfer._tcp.local.";
     let instance_name = "my-localTransfer-instance";
     let host_name = "my-localTransfer-host.local.";
-    let ip = local_ip()?;
+    let local_ip = local_ip_address::local_ip()?;
+
     
     if input == 1 {
         println!("Понял, я принимаю.\n Закончим настройку");
@@ -38,30 +38,28 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
 
 
         println!("запускаю ");
-        let stream = rx.iter().next().unwrap();
-         println!("запускаю2 ");
-         let local_addr = stream.local_addr().unwrap();
-        
-        
+        let stream = rx.iter().next().ok_or("ерр")?;
+        println!("запускаю2 ");
+        let local_addr = stream.local_addr()?;
         
 
-        
-        let mdns = ServiceDaemon::new().unwrap();
-        println!("мднс {:?} ", mdns.monitor());
-        
-        let servise_info = ServiceInfo::new(service_type, instance_name, host_name, local_addr.ip(), local_addr.port(), None).unwrap();
-        println!("serv info {:?} ", servise_info);
-        mdns.register(servise_info).unwrap();
-        println!("мднс зареган ");
 
-        let mut stream = stream.accept().unwrap();
+        
+        let mdns = ServiceDaemon::new()?; 
+        println!("мднс {:#?}", mdns.monitor());
+        let servise_info = ServiceInfo::new(service_type, instance_name, host_name, local_ip, local_addr.port(), None)?;
+        println!("мднс зареган {:#?}", servise_info);
+        mdns.register(servise_info)?;
+        
+        
+        
+        let mut stream = stream.accept()?;
 
         println!("беб {:?}", &stream.0);
-        getting_file(&mut stream.0, &path).unwrap();
-        
-        
 
-        tcp_handle.join().unwrap();
+        getting_file(&mut stream.0, &path)?;
+
+        let _ = tcp_handle.join().map_err(|e| println!("не удалось обработать поток: {:?}", e));
    
         
     } else {
@@ -72,6 +70,9 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
         let mut path =String::new();
         io::stdin().read_line(&mut path).expect("Не строка");
         if !has_path(&path){println!("Не путь!"); return Err(Box::new(std::io::Error::new(io::ErrorKind::IsADirectory, "Не путь!")));}
+
+       // if !fs::metadata(&path).is_ok() {println!("Файл не найден ");  return Err(Box::new(std::io::Error::new(io::ErrorKind::InvalidFilename, "Файл не найден!")));};
+        // C:\Users\макс4234\Desktop\brb\На экстренный случай.exe
         let path = path.trim();
 
         let mdns = ServiceDaemon::new()?;
@@ -79,8 +80,10 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
         let receiver = mdns.browse(service_type)?;
         println!("Ищу хост... ");
         
+         println!("Ищу хост проверка... {:?}", receiver);
         while let Ok(event) = receiver.recv() {
             match event {
+                
                 ServiceEvent::ServiceResolved(info) => {
                     println!("хост найден!");
                     let port = info.get_addresses_v4();
@@ -109,7 +112,8 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
 
 
 fn has_path(path: &str) -> bool{
-    path.contains(":\\")
+    let path = Path::new(path);
+    path.is_absolute()
 }
 
 fn create_stream() -> std::io::Result<TcpListener>{
@@ -121,6 +125,12 @@ fn create_stream() -> std::io::Result<TcpListener>{
 
 fn send_file(path: &str, mut stream: TcpStream) -> std::io::Result<()>{
     let mut file = File::open(path)?;
+    let file_name = std::path::Path::new(path).file_name().ok_or(io::Error::new(io::ErrorKind::NotFound,"Отсутсвует"))?.to_string_lossy();
+
+    let name_bytes = file_name.as_bytes();
+    stream.write_all(&(name_bytes.len() as u32).to_be_bytes())?;
+    stream.write_all(name_bytes)?;
+
     let bytes = file.metadata()?.len();
     stream.write_all(&bytes.to_be_bytes())?;
 
@@ -131,12 +141,20 @@ fn send_file(path: &str, mut stream: TcpStream) -> std::io::Result<()>{
 
 fn getting_file(stream: &mut TcpStream, path: &str) -> std::io::Result<()>{
     // TcpStream отдельно от TcpListener чтобы accept() не сбрасывался после окончания принятия
+    let mut len_buf = [0u8; 4];
+    stream.read_exact(&mut len_buf)?;
+    let name_len = u32::from_be_bytes(len_buf) as usize;
+
+    let mut name_buf = vec![0u8; name_len];
+    stream.read_exact(&mut name_buf)?;
+    let file_name = String::from_utf8_lossy(&name_buf);
+
     let mut buf = [0u8; 8];
     stream.read_exact(&mut buf)?;
     let file_len = u64::from_be_bytes(buf);
 
 
-    let mut file = File::create(path)?;
+    let mut file = File::create(format!("{}/{}", path, file_name))?;
     let mut limited_reader = stream.take(file_len);
 
     let bytes_written = std::io::copy(&mut limited_reader, &mut file)?;
@@ -183,7 +201,7 @@ fn server_test()-> std::io::Result<()>  {
     });
 
     thread::sleep(std::time::Duration::from_millis(50));
-    let mut client = TcpStream::connect("127.0.0.1:5656")?;
+    let client = TcpStream::connect("127.0.0.1:5656")?;
     send_file(temp_file_send.path().to_str().expect("Чета"), client)?;
     println!("[КЛИЕНТ] отправил: {:?}", temp_file_send);
     server.join().unwrap();
