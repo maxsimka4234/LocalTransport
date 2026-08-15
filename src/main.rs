0093fab72041f1277165b271715acf452d1ecac5
maxsimka <maxsimka4824@gmail.com>
2026-08-15 22:11:45 +0300

    some features

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::{ collections::HashSet, fs::{File}, io::{self, Read, Write}, net::{TcpListener, TcpStream}, path::{Path}, sync::mpsc, thread};
-use indicatif::ProgressBar;
+use std::{io::{self}, net::{TcpStream}, sync::mpsc, thread};
+
 use mdns_sd::{ServiceDaemon, ServiceEvent, ServiceInfo};
+use crate::stream::{real_addr, has_path, create_stream, getting_file, send_file};
+mod stream;
 
 fn hello() -> Result<(), Box<dyn std::error::Error>> {
     println!("Здраствуй! 1 - принимает файл (путь к итогу), 2 - отправляет файл (путь к файлу) ");
@@ -16,8 +18,8 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
     let local_ip = real_addr()?;
     //local_ip_address::local_ip()?; 
 
-    
-    if input == 1 {
+    match input {
+        1 => {
         println!("Понял, я принимаю.\n Закончим настройку");
         println!("Укажите путь принятия файла: ");
 
@@ -58,10 +60,10 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
         getting_file(&mut stream.0, &path)?;
 
         let _ = tcp_handle.join().map_err(|e| println!("не удалось обработать поток: {:?}", e));
-   
-        
-    } else {
-        //println!("Понял, принимаю {}", addr);
+
+        }
+        _ => {
+            //println!("Понял, принимаю {}", addr);
         println!("Понял, я отправляю.\n Закончим настройку");
         println!("Укажите путь отправки файла: ");
 
@@ -100,92 +102,8 @@ fn hello() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     
-    }
-    Ok(())
-}
-
-fn real_addr() -> std::io::Result<std::net::IpAddr>{
-    let interfaces = if_addrs::get_if_addrs()?;
-    let mut is_local = HashSet::new();
-    for interface in interfaces {
-        match interface.ip() {
-            std::net::IpAddr::V4(ipv4) => {
-                if ipv4.octets()[0] == 192 && ipv4.octets()[1] == 168 {
-                    is_local.insert(interface.ip());
-                }
-            }
-            std::net::IpAddr::V6(_) => {/*is_local.insert(interface.ip());*/}
         }
-       // if interface.ip().to_string().contains("192.168") {
-       //     println!("interface{:?}", interface)
-      //  }
     }
-    let interfaces:Vec<std::net::IpAddr> = is_local.into_iter().collect();
-    Ok(interfaces[rand::random_range(..interfaces.len())])
-}
-
-
-
-fn has_path(path: &str) -> bool{
-    let path = Path::new(path);
-    path.is_absolute()
-}
-
-fn create_stream() -> std::io::Result<TcpListener>{
-    let addr = "0.0.0.0:0";
-
-    TcpListener::bind(addr)
-}
-
-
-fn send_file(path: &str, mut stream: TcpStream) -> std::io::Result<()>{
-    let file = File::open(path)?;
-    let file_name = std::path::Path::new(path).file_name().ok_or(io::Error::new(io::ErrorKind::NotFound,"Отсутсвует"))?.to_string_lossy();
-
-    let name_bytes = file_name.as_bytes();
-    stream.write_all(&(name_bytes.len() as u32).to_be_bytes())?;
-    stream.write_all(name_bytes)?;
-
-    let bytes = file.metadata()?.len();
-    stream.write_all(&bytes.to_be_bytes())?;
-    
-    println!("Отправляю файл");
-
-    let pb = ProgressBar::new(bytes);
-    let mut file_reader = pb.wrap_read(file);
-    let bytes_copy = io::copy(&mut file_reader, &mut stream)?;
-
-    println!("Отправлено байт {}", bytes_copy);
-    Ok(())
-}
-
-fn getting_file(stream: &mut TcpStream, path: &str) -> std::io::Result<()>{
-    // TcpStream отдельно от TcpListener чтобы accept() не сбрасывался после окончания принятия
-    let mut len_buf = [0u8; 4];
-    stream.read_exact(&mut len_buf)?;
-    let name_len = u32::from_be_bytes(len_buf) as usize;
-
-    let mut name_buf = vec![0u8; name_len];
-    stream.read_exact(&mut name_buf)?;
-    let file_name = String::from_utf8_lossy(&name_buf);
-
-    let mut buf = [0u8; 8];
-    stream.read_exact(&mut buf)?;
-    let file_len = u64::from_be_bytes(buf);
-
-
-    let mut file = File::create(format!("{}/{}", path, file_name))?;
-    
-    let pb = ProgressBar::new(file_len);
-    let mut limited_reader = pb.wrap_read(stream.take(file_len));
-
-    let bytes_written = std::io::copy(&mut limited_reader, &mut file)?;
-
-    if bytes_written != file_len {
-        return Err(std::io::Error::new(std::io::ErrorKind::UnexpectedEof, "Ошибка в получении файла"));
-    }
-
-    println!("Файл получен!");
     Ok(())
 }
 
@@ -199,8 +117,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 
 #[test]
 fn server_test()-> std::io::Result<()>  {
+    use std::net::TcpListener;
     use std::thread;
     use std::fs;
+    use io::Write;
 
     let temp_file_get = tempfile::NamedTempFile::new()?;
     let mut temp_file_send = tempfile::NamedTempFile::new()?;
@@ -211,6 +131,8 @@ fn server_test()-> std::io::Result<()>  {
     let stream = TcpListener::bind("127.0.0.1:5656")?;
 
     let server = thread::spawn(move || {
+        use std::net::TcpListener;
+
         let mut stream = TcpListener::accept(&stream).expect("мсг");
         let pathi= &temp_file_get.path().to_str().expect("ее").to_string();
         //temp_file_get.close();
